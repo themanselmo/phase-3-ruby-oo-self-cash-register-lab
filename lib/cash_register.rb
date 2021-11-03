351c3064f70d51b0c119b9ef61b703a27423feda
max anselmo <itmanselmo@gmail.com>
2021-11-03 12:10:59 -0400

    assignment completed

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,59 @@
+require "pry"
+class CashRegister 
+  
+    # myRegister = CashRegister.new(10)
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @previous_total = 0
+        @items = []
+    end
 
+    def total=(newTotal)
+        @total = newTotal
+    end
+
+    def total
+        @total
+    end
+
+    def discount 
+        @discount
+    end
+
+    def items()
+        @items
+        # binding.pry
+    end
+
+def add_item(title, price, quantity = 1)
+        # binding.pry
+        @previous_total = @total
+        @total = @total + (price * quantity)
+        if(quantity > 1)
+            count = quantity
+            while count > 0 
+                @items << title
+                count -= 1
+            end
+        elsif(quantity == 1)
+            @items << title
+        end
+    end
+
+    def apply_discount
+        if(discount == 0)
+            "There is no discount to apply."
+        else
+            discount_amount = @discount * 10
+            @total = @total - discount_amount
+
+            "After the discount, the total comes to $#{total}."
+        end
+    end
+
+    def void_last_transaction
+        @total = @previous_total
+    end
+
+end
